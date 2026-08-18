6af2d450cac81734725382da8046f66211a5db1b
0arKes <proarKes@gmail.com>
2026-08-18 20:31:41 -0300

    feat: Cirando logica de registro
    Criando logica para registrar usuários.
    Criando formulário para registro.
    Criando rota simples para registro de usuários.
    Criando template e static simples para testar o registro.

---
+++ monitoring/forms/login_form.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
+class CreateUserForm(forms.Form):
+    email = forms.EmailField(
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={
+                "autocomplete": "email",
+                "placeholder": "Digite seu email",
+            }
+        ),
+    )
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={
+                "autocomplete": "username",
+                "placeholder": "Digite seu username",
+            }
+        ),
+    )
+    password = forms.CharField(
+        max_length=24,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "placeholder": "Digite sua senha",
+            }
+        ),
+    )
+    password_confirmation = forms.CharField(
+        max_length=24,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "placeholder": "Repita sua senha"}
+        ),
+    )
+
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data["email"]).exists():
+            raise ValidationError("Usuario com email já registrado!")
+
+        return self.cleaned_data["email"]
+
+    def clean_username(self):
+        if User.objects.filter(username=self.cleaned_data["username"]).exists():
+            raise ValidationError("Usuario com username já registrado!")
+
+        return self.cleaned_data["username"]
+
+    def clean_password(self):
+        password = self.cleaned_data["password"]
+        validate_password(password)
+
+        return password
+
+    def clean(self):
+        data = super().clean()
+
+        password = data.get("password")
+        password_confirmation = data.get("password_confirmation")
+
+        if password != password_confirmation:
+            raise ValidationError("Senhas não coincidem")
+
+        return data
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from .forms.login_form import CreateUserForm
+
+
 # Create your views here.
+def register_user(request):
+
+    form_user = CreateUserForm()
+
+    if request.method == "POST":
+        form_user = CreateUserForm(request.POST)
+        if form_user.is_valid():
+            form = form_user.cleaned_data
+            User.objects.create_user(
+                username=form["username"],
+                email=form["email"],
+                password=form["password"],
+            )
+
+        return render(request, "monitoring/login.html", {"form": form_user})
+
+    return render(request, "monitoring/login.html", {"form": form_user})
